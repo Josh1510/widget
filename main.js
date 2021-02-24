3630edf74db625daf93aa006c33e451475b78ea7
josh1510 <joshmoon.1510@gmail.com>
2021-02-24 14:52:11 +1100

    n/a

--- main.js
+++ main.js
@@ -2,35 +2,17 @@
 //     document.querySelectorAll('a.ot-select-label.ot-dtp-picker-selector-link')
 // );
 $(window).on('load', function () {
-    // code here
-
     var selection = document.querySelectorAll(
         'a.ot-select-label.ot-dtp-picker-selector-link'
     );
     console.log(selection);
-    selection = selection[0];
-    //selection = selection[0].childNodes[0].nodeValue;
 
-    //selection[0].childNodes[0].nodeValue = '12:00 PM';
+    //changes text of time selection _only_, does not update actual value.
+    //selection[0].childNodes[0].nodeValue = '3:00 PM';
 
-    // var standardTime = selection.innerHTML;
-    // selection.innerHTML = '2:00 PM';
-    //selection.innerText = '2:00 PM';
+    //Works to select/highlight the time but does not pass it on
+    $('select>option[value="15:00"]').prop('selected', 'selected');
 
-    $('select>option[value="12:00"]').prop('selected', 'selected');
-
-    // $(
-    //     '.a.ot-select-label.ot-dtp-picker-selector-link > select option[value="15:30"]'
-    // );
-
-    // $(
-    //     '.a.ot-select-label.ot-dtp-picker-selector-link option[value="14:00"]'
-    // ).prop('selected', true);
-
-    // $(
-    //     '.a.ot-select-label.ot-dtp-picker-selector-link option[value="14:00"]'
-    // ).attr('selected', 'selected');
-
-    console.log(selection);
+    // console.log(selection);
     //console.log(standardTime);
 });
