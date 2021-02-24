3aba1603b8d02546243454b18efa1637295fca37
josh1510 <joshmoon.1510@gmail.com>
2021-02-24 15:41:35 +1100

    updates selection selection variable to only target time

--- main.js
+++ main.js
@@ -2,17 +2,20 @@
 //     document.querySelectorAll('a.ot-select-label.ot-dtp-picker-selector-link')
 // );
 $(window).on('load', function () {
-    var selection = document.querySelectorAll(
-        'a.ot-select-label.ot-dtp-picker-selector-link'
-    );
-    console.log(selection);
+    // Changes both time and party size text
+    //$('a.ot-select-label.ot-dtp-picker-selector-link').text('test');
+
+    //changes only the text of the selection box.
+    var selection = $('a.ot-select-label.ot-dtp-picker-selector-link');
+    selection[0].childNodes[0].nodeValue = '4:00 PM';
+    //selection.text('hello');
 
     //changes text of time selection _only_, does not update actual value.
-    //selection[0].childNodes[0].nodeValue = '3:00 PM';
+    //selection[0].childNodes[0].nodeValue = '4:00 PM';
 
     //Works to select/highlight the time but does not pass it on
-    $('select>option[value="15:00"]').prop('selected', 'selected');
+    $('select option[value="16:00"]').prop('selected', true);
 
-    // console.log(selection);
+    console.log(selection);
     //console.log(standardTime);
 });
