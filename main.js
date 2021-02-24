84a92b5a3e1088cbe4461433852ca48d9c9a4497
josh1510 <joshmoon.1510@gmail.com>
2021-02-24 17:33:44 +1100

    adding .change() to select to update the selection

--- main.js
+++ main.js
@@ -6,16 +6,15 @@ $(window).on('load', function () {
     //$('a.ot-select-label.ot-dtp-picker-selector-link').text('test');
 
     //changes only the text of the selection box.
-    var selection = $('a.ot-select-label.ot-dtp-picker-selector-link');
-    selection[0].childNodes[0].nodeValue = '4:00 PM';
+    // var selection = $('a.ot-select-label.ot-dtp-picker-selector-link');
+    // selection[0].childNodes[0].nodeValue = '4:00 PM';
     //selection.text('hello');
 
     //changes text of time selection _only_, does not update actual value.
     //selection[0].childNodes[0].nodeValue = '4:00 PM';
 
-    //Works to select/highlight the time but does not pass it on
-    $('select option[value="16:00"]').prop('selected', true);
-
-    console.log(selection);
-    //console.log(standardTime);
+    //Both of the below options work to select the correct time.
+    //If the time has passed then selects default of 7pm
+    $('select option[value="22:00"]').prop('selected', true).change();
+    //$('select[name="Select_0"]').val('22:00').change();
 });
