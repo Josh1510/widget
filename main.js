50ac117b337ad863b84f87c165d3124db9de7b99
josh1510 <joshmoon.1510@gmail.com>
2021-02-25 11:48:13 +1100

    Added long if else to select later times if it is after mid day

--- main.js
+++ main.js
@@ -15,6 +15,38 @@ $(window).on('load', function () {
 
     //Both of the below options work to select the correct time.
     //If the time has passed then selects default of 7pm
-    $('select option[value="22:00"]').prop('selected', true).change();
+    // if ($('select option[value="12:00"]').length) {
+    //     $('select option[value="12:00"]').prop('selected', true).change();
+    // } else {
+    //     console.log('not here');
+    //     $('select option[value="12:00"]').append('Some appended text.');
+    // }
+
     //$('select[name="Select_0"]').val('22:00').change();
+    let selector = $('select[name="Select_0"]');
+    if ($('select option[value="12:00"]').length) {
+        selector.val('12:00').change();
+    } else if ($('select option[value="12:30"]').length) {
+        selector.val('12:30').change();
+    } else if ($('select option[value="13:00"]').length) {
+        selector.val('13:00').change();
+    } else if ($('select option[value="13:30"]').length) {
+        selector.val('13:30').change();
+    } else if ($('select option[value="14:00"]').length) {
+        selector.val('14:00').change();
+    } else if ($('select option[value="14:30"]').length) {
+        selector.val('14:30').change();
+    } else if ($('select option[value="15:00"]').length) {
+        selector.val('15:00').change();
+    } else if ($('select option[value="15:30"]').length) {
+        selector.val('15:30').change();
+    } else if ($('select option[value="16:00"]').length) {
+        selector.val('16:00').change();
+    } else if ($('select option[value="17:30"]').length) {
+        selector.val('17:30').change();
+    } else if ($('select option[value="18:00"]').length) {
+        selector.val('18:00').change();
+    } else if ($('select option[value="18:30"]').length) {
+        selector.val('18:30').change();
+    }
 });
